a7e82b85cdabc1f2f01bca7fae5c6af0e4da9254
jong-k <fully.charged07@gmail.com>
2024-09-29 15:42:35 +0900

    feat: mobile container 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,8 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 maximum-scale=1"
+        />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <div className="mx-auto max-w-[600px]">{children}</div>
       </body>
     </html>
   );
