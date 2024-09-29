864a2d34f0535f88a12092eb34d2a8cceb77d76c
jong-k <fully.charged07@gmail.com>
2024-09-29 17:36:58 +0900

    chore: eslint tailwind plugin 설치

--- src/components/base/Container.tsx
+++ src/components/base/Container.tsx
@@ -1,5 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mx-auto min-h-screen max-w-[600px] bg-background">{children}</div>
-  );
+  return <div className="mx-auto min-h-screen max-w-[600px] bg-zinc-100">{children}</div>;
 }
--- src/components/base/GNB.tsx
+++ src/components/base/GNB.tsx
@@ -1,3 +1,17 @@
+"use client";
+import Link from "next/link";
+
 export default function GNB() {
-  return <nav className="h-gnb bg-black text-white">겁쟁이 추적기</nav>;
+  return (
+    <nav className="flex h-gnb items-center justify-between bg-primary px-3 text-white">
+      <Link href="/">
+        <span className="text-xl font-semibold">겁쟁이 추적기</span>
+      </Link>
+      <div>
+        <div className="cursor-pointer" onClick={() => alert("기능 준비중")}>
+          설정
+        </div>
+      </div>
+    </nav>
+  );
 }
