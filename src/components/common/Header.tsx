6d698dba87ec4c0affb60dba6820ef18cdaea069
jong-k <fully.charged07@gmail.com>
2024-10-09 11:54:53 +0900

    chore: eslint-plugin-tailwindcss 라이브러리 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import HeaderNav from "./HeaderNav";
 
 export default function Header() {
   return (
-    <header className="z-header fixed inset-x-0 top-0 mx-auto w-[600px]">
+    <header className="fixed inset-x-0 top-0 z-header mx-auto w-[600px]">
       <HeaderNav />
     </header>
   );
