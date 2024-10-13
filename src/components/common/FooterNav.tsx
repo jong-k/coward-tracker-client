072f7fc5c20246a5b286968ccef3aa435249ec97
jong-k <fully.charged07@gmail.com>
2024-10-13 23:18:21 +0900

    feat: 네비게이션 푸터 추가

--- src/components/common/FooterNav.tsx
+++ src/components/common/FooterNav.tsx
@@ -1,10 +1,28 @@
+import Link from "next/link";
+
 export default function FooterNav() {
   return (
-    <nav className="flex h-footer text-white">
-      <div>홈</div>
-      <div>통계</div>
-      <div>분석</div>
-      <div>설정</div>
+    <nav className="flex h-footer items-center text-white">
+      <div className="flex-1 text-center text-lg font-semibold">
+        <Link className="p-4" href="/">
+          홈
+        </Link>
+      </div>
+      <div className="flex-1 text-center text-lg font-semibold">
+        <Link className="p-4" href="/">
+          통계
+        </Link>
+      </div>
+      <div className="flex-1 text-center text-lg font-semibold">
+        <Link className="p-4" href="/">
+          분석
+        </Link>
+      </div>
+      <div className="flex-1 text-center text-lg font-semibold">
+        <Link className="p-4" href="/">
+          설정
+        </Link>
+      </div>
     </nav>
   );
 }
